7913adffa4e7a74e34e768e49464411c719273c4
Erik Uecke <erikuecke@gmail.com>
2018-02-27 15:46:49 -0500

    Add a dropPin for longGesture.

--- testMapBoxNavigation/ViewController.swift
+++ testMapBoxNavigation/ViewController.swift
@@ -28,8 +28,31 @@ class ViewController: UIViewController, MGLMapViewDelegate {
         // Set the map view's delegate
         mapView.delegate = self
         
+        // Allow the map to display the user's location
+        mapView.showsUserLocation = true
+        mapView.setUserTrackingMode(.follow, animated: true)
+        
+        // Add a gesture recognizer to the map view.
+        let setDestination = UILongPressGestureRecognizer(target: self, action: #selector(didLongPress(_:)))
+        mapView.addGestureRecognizer(setDestination)
+        
     }
-
+    
+    @objc func didLongPress(_ sender: UILongPressGestureRecognizer) {
+        guard sender.state == .began else { return }
+        
+        // Converts point where user did a long press to map coordinates
+        let point = sender.location(in: mapView)
+        let coordinate = mapView.convert(point, toCoordinateFrom: mapView)
+        
+    
+        // Create a basic point annotation and add it to the map
+        let annotation = MGLPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = "Start navigation"
+        mapView.addAnnotation(annotation)
+        
+    }
 
 }
 
